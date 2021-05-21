f8f04f6072defa9a50644dea735f7e2f7a2586a1
Eiyuulia <okkyprasetian@gmail.com>
2021-05-21 20:59:19 +0700

    edit tes8

--- tes8/script.js
+++ tes8/script.js
@@ -8,3 +8,6 @@ judul.style.color = 'salmon';
 
 const link = document.querySelector('#container #a a');
 link.setAttribute('href','https://github.com/Eiyuulia');
+
+const body = document.getElementsByTagName('body')[0];
+body.style.border = '2px solid lime';
