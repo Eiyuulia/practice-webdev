2d0f3a0139451d3de08667f84d8e0d495722f435
Eiyuulia <okkyprasetian@gmail.com>
2021-05-21 20:21:10 +0700

    tambah script tes7

--- tes7/script.js
+++ tes7/script.js
@@ -1,22 +1,29 @@
 // DOM SELECTION
 
-// document.getElementById() -> return element
+// // document.getElementById() -> return element
 
-const judul = document.getElementById("judul");
-judul.style.color = "blue";
-judul.style.backgroundColor = "grey";
-judul.innerHTML = "Tes Tes";
+// const judul = document.getElementById("judul");
+// judul.style.color = "blue";
+// judul.style.backgroundColor = "grey";
+// judul.innerHTML = "Tes Tes";
 
 
-// document.getElementsByTagName() -> HTMLCollection
+// // document.getElementsByTagName() -> HTMLCollection
 
-const p = document.getElementsByTagName("p");
+// const p = document.getElementsByTagName("p");
 
-for(let i = 0; i < p.length; i++){
-    p[i].style.backgroundColor = "lightblue";
-}
+// for(let i = 0; i < p.length; i++){
+//     p[i].style.backgroundColor = "lightblue";
+// }
 
-// document.getElementsByClassName() -> HTMLCollection
+// // document.getElementsByClassName() -> HTMLCollection
 
-const p1 = document.getElementsByClassName("p1");
-p1[0].innerHTML = "coba ubah oi oi";
+// const p1 = document.getElementsByClassName("p1");
+// p1[0].innerHTML = "coba ubah oi oi";
+
+// --------------------
+
+// document.querySelector()
+
+const p4 = document.querySelector('#b p');
+p4.innerHTML = "ini paragraph 4 yg diubah oleh script";
