b9288c279fd36e1d2cceaa333b604f35639978f0
Eiyuulia <okkyprasetian@gmail.com>
2021-05-21 20:46:49 +0700

    edit tes8

--- tes8/script.js
+++ tes8/script.js
@@ -1,5 +1,7 @@
 const judul = document.getElementById('judul');
 judul.innerHTML = 'Okky Prasetia';
 
-const secA = document.querySelector('#a');
-secA.innerHTML = 'Hai semua';
+// const secA = document.querySelector('#a');
+// secA.innerHTML = 'Hai semua';
+
+judul.style.color = 'salmon';
