0143841d1dacff382393c34a11882c626d5b60fd
Eiyuulia <okkyprasetian@gmail.com>
2021-05-21 13:40:58 +0700

    tambah tambah

--- tes7/script.js
+++ tes7/script.js
@@ -4,3 +4,19 @@
 
 const judul = document.getElementById("judul");
 judul.style.color = "blue";
+judul.style.backgroundColor = "grey";
+judul.innerHTML = "Tes Tes";
+
+
+// document.getElementsByTagName() -> HTMLCollection
+
+const p = document.getElementsByTagName("p");
+
+for(let i = 0; i < p.length; i++){
+    p[i].style.backgroundColor = "lightblue";
+}
+
+// document.getElementsByClassName() -> HTMLCollection
+
+const p1 = document.getElementsByClassName("p1");
+p1[0].innerHTML = "coba ubah oi oi";
