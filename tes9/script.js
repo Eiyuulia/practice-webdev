21b685e3f2468ff955584bef1fe5b3f860019a3a
Eiyuulia <okkyprasetian@gmail.com>
2021-05-22 11:29:12 +0700

    edit script tes9

--- tes9/script.js
+++ tes9/script.js
@@ -1,1 +1,11 @@
 // DOM manipulation
+
+// membuat element/tag baru
+const pBaru = document.createElement('p');
+
+// membuat text/content untuk tag/ node
+const textPBaru = document.createTextNode('Paragrag baru');
+
+//  simpan tulisan/node kedalam paragraf/element
+pBaru.appendChild(textPBaru);
+
