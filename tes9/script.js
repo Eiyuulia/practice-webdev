e3b5d6e40f1410d7afb1f39d6f436f214fa45e80
Eiyuulia <okkyprasetian@gmail.com>
2021-05-22 11:33:56 +0700

    edit script tes9

--- tes9/script.js
+++ tes9/script.js
@@ -3,9 +3,21 @@
 // membuat element/tag baru
 const pBaru = document.createElement('p');
 
-// membuat text/content untuk tag/ node
+// membuat text/content untuk tag/node baru
 const textPBaru = document.createTextNode('Paragrag baru');
 
 //  simpan tulisan/node kedalam paragraf/element
 pBaru.appendChild(textPBaru);
 
+// simpan tag kedalam halaman dengan appendchild
+const sectionA = document.getElementById('a');
+sectionA.appendChild(pBaru);
+
+
+
+
+
+
+const liBaru = document.createElement('li');
+const textLiBaru = document.createTextNode('item baru');
+liBaru.appendChild(textLiBaru);
